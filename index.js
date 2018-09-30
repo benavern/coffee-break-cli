e621a069994ac87f42e793aeff7a0186d672afb3
Benjamin Caradeuc <bencrdc@gmail.com>
2018-09-30 14:41:27 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk')
+const clear = require('clear')
+const figlet = require('figlet')
+const inquirer = require('./lib/inquirer')
+const breakTime = require('./lib/breakTime')
+
+const clearPage = () => {
+    clear()
+    console.log(
+        chalk.yellow(
+            figlet.textSync('Coffee Break', { horizontalLayout: 'full' })
+        )
+    )
+}
+
+const run = async () => {
+    clearPage()
+    const choice = await inquirer.choose()
+
+    let settings
+    if (choice) {
+        settings = choice
+    } else {
+        clearPage()
+        settings = await inquirer.settings()
+        clearPage()
+        const save = await inquirer.save(settings)
+    }
+
+    breakTime.start(settings)
+}
+
+run()
